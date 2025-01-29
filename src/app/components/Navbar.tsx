8aef9eb1c5887264b69c39d191400fc12ff7a8f4
George Geru <george.g@machinas.com>
2025-01-29 22:09:46 +0200

    navbar changed

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -4,13 +4,16 @@ import { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { navbarLinks } from '../utils/navbarLinks';
-import { signOut } from 'next-auth/react';
+import { signOut, useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 
 const Navbar = () => {
     const [menuOpen, setMenuOpen] = useState<boolean>(false);
+    const { data: session } = useSession();
+    const router = useRouter();
 
-    const handleLogout = async () => {
-        await signOut();
+    const handleLogout = () => {
+        signOut({ redirect: false }).then(() => router.push('/'));
     };
 
     return (
@@ -32,34 +35,14 @@ const Navbar = () => {
                     </div>
                 </Link>
 
-                <button
-                    onClick={() => setMenuOpen((prevState) => !prevState)}
-                    className="block md:hidden text-white"
-                    aria-label="Toggle Menu"
-                >
-                    <Image src="/menu.svg" alt="Menu" width={40} height={40} />
-                </button>
-            </div>
-
-            <div className="block md:hidden">
-                {menuOpen && (
-                    <div
-                        className="fixed inset-0 bg-black opacity-50 z-10"
-                        onClick={() => setMenuOpen((prevState) => !prevState)}
-                    ></div>
-                )}
-
-                <div
-                    className={`fixed top-0 right-0 bottom-0 w-64 bg-[#0C9DDA] z-20 flex flex-col transition-all duration-300 ease-in-out transform ${
-                        menuOpen ? 'translate-x-0' : 'translate-x-full'
-                    }`}
-                >
-                    <ul className="flex flex-col space-y-4 p-4 flex-grow">
+                {session && (
+                    <div className="flex space-x-4 p-4">
                         {navbarLinks.map((link, index) => (
-                            <li key={index} className="p-2">
+                            <>
                                 {!link.isButton && (
                                     <Link
                                         href={link.href}
+                                        key={index}
                                         className="text-white hover:text-gray-200 flex gap-2 items-center"
                                     >
                                         <Image
@@ -69,17 +52,13 @@ const Navbar = () => {
                                             height={32}
                                             className="object-cover group-hover:scale-110 transition-transform"
                                         />
-                                        <span>{link.alt}</span>
                                     </Link>
                                 )}
-                            </li>
+                            </>
                         ))}
-                    </ul>
-
-                    <div className="flex justify-end items-center p-4">
                         <button
                             onClick={handleLogout}
-                            className="text-white hover:text-gray-200 p-4"
+                            className="text-white hover:text-gray-200"
                         >
                             <Image
                                 src="/logout.svg"
@@ -90,8 +69,79 @@ const Navbar = () => {
                             />
                         </button>
                     </div>
-                </div>
+                )}
+
+                {session && (
+                    <button
+                        onClick={() => setMenuOpen((prevState) => !prevState)}
+                        className="block md:hidden text-white"
+                        aria-label="Toggle Menu"
+                    >
+                        <Image
+                            src="/menu.svg"
+                            alt="Menu"
+                            width={40}
+                            height={40}
+                        />
+                    </button>
+                )}
             </div>
+
+            {session && (
+                <div className="block md:hidden">
+                    {menuOpen && (
+                        <div
+                            className="fixed inset-0 bg-black opacity-50 z-10"
+                            onClick={() =>
+                                setMenuOpen((prevState) => !prevState)
+                            }
+                        ></div>
+                    )}
+
+                    <div
+                        className={`fixed top-0 right-0 bottom-0 w-64 bg-[#0C9DDA] z-20 flex flex-col transition-all duration-300 ease-in-out transform ${
+                            menuOpen ? 'translate-x-0' : 'translate-x-full'
+                        }`}
+                    >
+                        <ul className="flex flex-col space-y-4 p-4 flex-grow">
+                            {navbarLinks.map((link, index) => (
+                                <li key={index} className="p-2">
+                                    {!link.isButton && (
+                                        <Link
+                                            href={link.href}
+                                            className="text-white hover:text-gray-200 flex gap-2 items-center"
+                                        >
+                                            <Image
+                                                src={link.src}
+                                                alt={link.alt}
+                                                width={32}
+                                                height={32}
+                                                className="object-cover group-hover:scale-110 transition-transform"
+                                            />
+                                            <span>{link.alt}</span>
+                                        </Link>
+                                    )}
+                                </li>
+                            ))}
+                        </ul>
+
+                        <div className="flex justify-end items-center p-4">
+                            <button
+                                onClick={handleLogout}
+                                className="text-white hover:text-gray-200 p-4"
+                            >
+                                <Image
+                                    src="/logout.svg"
+                                    alt="Logout"
+                                    width={32}
+                                    height={32}
+                                    className="object-cover group-hover:scale-110 transition-transform"
+                                />
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
